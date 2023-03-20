99e8e029b5cd694cf7550b98ed420ad00762d38f
Nischal _Shakya <81230596+nischal55@users.noreply.github.com>
2023-03-20 16:10:55 +0545

    Add files via upload

---
+++ Nischal/backend/config/db.php
@@ -0,0 +1,7 @@
+<?php
+$host= "localhost";
+$user= "root";
+$pass="";
+$dbname="contact_425";
+$conn = mysqli_connect($host,$user,$pass,$dbname)or die();
+?>
---
+++ Nischal/backend/functions/login_module.php
@@ -0,0 +1,21 @@
+<?php
+$functionName = $_POST['funName'];
+
+if ($functionName == "login_function") {
+    login_function();
+}
+
+function login_function()
+{
+    $username = "Nischal65";
+    $password = "Nischal123";
+    $input_username = $_POST['username'];
+    $input_password = $_POST['password'];
+    if ($username == $input_username) {
+        if ($password == $input_password) {
+            echo ("login_success");
+        } else {
+            echo ("login_fail");
+        }
+    }
+}
